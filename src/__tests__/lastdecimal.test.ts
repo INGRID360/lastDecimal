365c618d1a23712359efe51e5bfa0e5796fd22bd
Mathias Åsberg <mathias.asberg@gmail.com>
2018-10-14 15:50:57 +0200

    sunc

--- src/__tests__/lastdecimal.test.ts
+++ src/__tests__/lastdecimal.test.ts
@@ -1,16 +1,16 @@
-import { lastDecimal } from '../index';
-test('lastDecimal', () => {
-  expect(lastDecimal(555.555, true)).toBe('555.554');
+import { lastdecimal } from '../index';
+test('lastdecimal', () => {
+  expect(lastdecimal(555.555, true)).toBe('555.554');
 });
 
-test('lastDecimal', () => {
-  expect(lastDecimal(5.555421, true)).toBe('5.555420');
+test('lastdecimal', () => {
+  expect(lastdecimal(5.555421, true)).toBe('5.555420');
 });
 
-test('lastDecimal', () => {
-  expect(lastDecimal(555.555, false)).toBe('555.556');
+test('lastdecimal', () => {
+  expect(lastdecimal(555.555, false)).toBe('555.556');
 });
 
-test('lastDecimal', () => {
-  expect(lastDecimal(5.321312399, false)).toBe('5.321312400');
+test('lastdecimal', () => {
+  expect(lastdecimal(5.321312399, false)).toBe('5.321312400');
 });
